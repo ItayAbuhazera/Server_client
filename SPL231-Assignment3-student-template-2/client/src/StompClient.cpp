f4958aa76588bfc23cd2556a4470f54f85f2b646
Or Malky <malky@post.bgu.ac.il>
2023-01-09 20:18:51 +0200

    Co-authored-by: ItayAbuhazera <ItayAbuhazera@users.noreply.github.com>

--- SPL231-Assignment3-student-template-2/client/src/StompClient.cpp
+++ SPL231-Assignment3-student-template-2/client/src/StompClient.cpp
@@ -1,4 +1,35 @@
+#include <stdlib.h>
+#include "../include/StompProtocol.h"
+//#include "../include/KeyboardThread.h"
+#include <thread>
+#include <string>
+
+
+#include <iostream>;
+
 int main(int argc, char *argv[]) {
-	// TODO: implement the STOMP client
+	ConnectionHandler* ch = new ConnectionHandler();
+
+    StompProtocol* stompProtocol  = new StompProtocol(*ch);
+    KeyboardThread  keyThread(*ch, *stompProtocol);
+    std::thread th(&KeyboardThread::run,&keyThread);
+    while(1){
+        if(ch->isConnected()) {
+            std::string ans;
+            if (!ch->getFrameAscii(ans, '\0')) {
+                std::cout << "Disconnected. Exiting...\n" << std::endl;
+                break;
+            }
+            std::string out = stompProtocol->processFrame(ans);
+            if (out != "")
+                if (!ch->sendFrameAscii(out,'\0')) {
+                    std::cout << "Disconnected. Exiting...\n" << std::endl;
+                    break;
+                }
+        }
+    }
+    th.join();
+    delete(ch);
+    delete(stompProtocol);
 	return 0;
 }
